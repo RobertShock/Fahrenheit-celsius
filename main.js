38463c7d21f51c0fb3c2a79fb414d5187ab049a2
Robbie Shock <rob.f.shock@gmail.com>
2017-08-07 18:10:49 -0500

    added additional code

--- main.js
+++ main.js
@@ -1,59 +1,40 @@
-function tempConversion(form) {
-    var Fi = form.fahr.value;
-    var Ci = form.cels.value;
-    var C2, F2, C, F;
+var temperatures = function(X) {
+        var output, userInput, result, opposite;
+        userInput = document.getElementById('temp').value;
+        if (X === "C") {
+            result = 9 / 5 * (userInput) + 32;
+            opposite = "F";
+        } 
+        else {
+            result = (userInput - 32) / 9 *5;
+            opposite = "C";
+        }
+        if (result % 1 !== 0) {
+            console.log(result % 1);
+            result = result.toFixed(2);
+        }
+        if (userInput || userInput === "0") {
+            output = userInput + " \°" + X + " is equal to " + result + " \°" + opposite;
+        } 
+           if (output > 90){
+            (document.getElementById("output").classList.add('redText'));
+            }
 
-    if (Ci != "" && Fi != "") {
-  alert("You must clear the entries before attempting another conversion");
-  return;
-
-    } else if (Ci == "" && Fi == "") {
-  alert("You have not input a temperature");
-        return;
-
-    } else if (Ci == "") {
-  F = parseFloat(Fi);
-  C = (5/9) * (F - 32);
-
-        if (C < 0.01 && C > -0.01) {
-      form.cels.value = 0;
-
-  } else {
-  
-      if (C < 0) {
-        C2 = parseInt((C * 100) - 0.5);
-    
-      } else if (C > 0) {
-        C2 = parseInt((C * 100) + 0.5);
-      }
-
-      C2 /= 100;
-          form.cels.value = C2; 
-  }
-
-   } else if (Fi == "") {
-  C = parseFloat(Ci);
-  F = (9/5 * C) + 32;
-
-  if (F < 0.01 && F > -0.01) {
-            form.fahr.value = 0;
-
-  } else {
-  
-      if (F < 0) {
-        F2 = parseInt((F * 100) - 0.5);
-    
-      } else if (F > 0) {
-        F2 = parseInt((F * 100) + 0.5);
-      }
-
-      F2 /= 100;
-      form.fahr.value= F2;  
-  }
+        document.querySelector('output').innerHTML = output;
     }
+document.getElementById('ctof').addEventListener('click', function(){temperatures('C');})
+document.getElementById('ctof2').addEventListener('click', function(){temperatures('F');})
+
+
+function clearForm() {
+  document.getElementById("myForm").reset();
 }
 
-function wipe(form) {
-    form.fahr.value = ""    
-    form.cels.value = ""    
-}
+
+// Create a block level element that will hold the text of the converted temperature.
+// Create a button that, when clicked, displays the converted temperature.
+// Create another button that, when clicked, clears any text in the input field.
+// Add an event handler to the input field that checks if the user pressed the enter key, and if that happens, perform the conversion.
+// If the temperature is greater than 90F/32C the color of the converted temperature should be red.
+// If the temperature is less than 32F/0C the color of the converted temperature should be blue.
+// For any other temperature, the color should be green.
